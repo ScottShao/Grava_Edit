028c2d9fed32c89437a42332fbc590d5afbcd991
ZhaoyangShao <zhaoyang@ualberta.ca>
2016-11-08 10:30:09 -0700

    gz

--- src/main/java/eu/unitn/disi/db/grava/utils/ReadGZFile.java
+++ src/main/java/eu/unitn/disi/db/grava/utils/ReadGZFile.java
@@ -46,23 +46,14 @@ public class ReadGZFile {
 				String[] words = line.split("\\t");
 				if (words.length < 3) continue;
 				System.out.println(line);
-				System.out.println("first " + words[0]);
-				System.out.println("second " + words[1]);
-				System.out.println("third " + words[2]);
-				
-//				int zero = 0;
-//				while (zero < words.length && words[zero].length() == 0) zero++;
-//				int first = zero + 1;
-//				while (first < words.length && words[first].length() == 0) first++;
-//				int second = first + 1;
-//				while (second < words.length && words[second].length() == 0) second++;
-//				if (second >= words.length) continue;
-//				if (words[zero].contains("m.") && words[first].contains("name") && words[second].contains("@en")) {
-//					String[] a = words[zero].split("/");
-//					bw.write(a[a.length - 1] + " " + words[second].split("\"")[1]);
-//					bw.newLine();
-//				}
-//				if (count % 100000 == 0) System.out.println("Process " + count + " lines");
+
+				if (words[0].contains("m.") && words[1].contains("name") && words[2].contains("@en")) {
+					String[] a = words[0].split("/");
+					System.out.println(a[a.length - 1].substring(0, a[a.length - 1].length() - 1) + " " + words[2].substring(1, words[2].length() - 4));
+					bw.write(a[a.length - 1].substring(0, a[a.length - 1].length() - 1) + " " + words[2].substring(1, words[2].length() - 4));
+					bw.newLine();
+				}
+				if (count % 100000 == 0) System.out.println("Process " + count + " lines");
 				count++;
 			}
 		} catch (IOException e) {
@@ -82,9 +73,9 @@ public class ReadGZFile {
 	 * @throws IOException 
 	 */
 	public static void main(String[] args) throws IOException {
-//		String a = "masdas";
-//		System.out.println(a.contains("m."));
-		ReadGZFile rf = new ReadGZFile("freebase.gz");
+		String a = "<http://rdf.freebase.com/ns/award.award_winner>";
+		System.out.println(a.split("/")[a.split("/").length-1]);
+//		ReadGZFile rf = new ReadGZFile("freebase.gz");
 //		String a = "<http://rdf.freebase.com/ns/american_football.football_player.footballdb_id>    <http://www.w3.org/2000/01/rdf-schema#label>    \"footballdb ID\"@en      .";
 //		System.out.println(a.split(" ")[2]);
 	}
